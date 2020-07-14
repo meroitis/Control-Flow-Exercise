4a921e1d6d4e813b5c765a3749c3834a8af0f7bb
meroitis <67976213+meroitis@users.noreply.github.com>
2020-07-14 15:04:13 -0500

    Add files via upload

---
+++ Cristobal_R_ControlFlowExercise.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+// Control Flow Exercise
+// Varibales and Constants
+var seconds = 180
+
+// Control Flow
+for index in 1...180 {
+    print("\(index) seconds ")
+}
+
+// Prints
+if seconds <= 180 {
+    print("Egg is ready take out now")
+}
